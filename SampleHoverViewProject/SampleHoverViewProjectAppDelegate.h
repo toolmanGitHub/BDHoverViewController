1daa4651dd3915b2bff932f58f8b3065f277959c
Tim Taylor <timthetoolmantaylor@gmail.com>
2011-08-27 14:19:46 -0700

    Initial Commit

---
+++ SampleHoverViewProject/SampleHoverViewProjectAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SampleHoverViewProjectAppDelegate.h
+//  SampleHoverViewProject
+//
+//  Created by Tim and Jennifer Taylor on 8/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SampleHoverViewProjectAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ SampleHoverViewProject/iPhone/SampleHoverViewProjectAppDelegate_iPhone.m
@@ -0,0 +1,13 @@
+//
+//  SampleHoverViewProjectAppDelegate_iPhone.m
+//  SampleHoverViewProject
+//
+//  Created by Tim and Jennifer Taylor on 8/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SampleHoverViewProjectAppDelegate_iPhone.h"
+
+@implementation SampleHoverViewProjectAppDelegate_iPhone
+
+@end
